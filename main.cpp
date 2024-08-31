9161c951f4c816cda53fd14ea4d757ec12bae071
Emsonek <chmielek1924@gmail.com>
2024-08-31 17:23:30 +0200

    mhm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-	std::cout<<"SFML RPG game project"<<std::endl;
-	std::cout<<"Simpel changes from first"<<std::endl;
-	std::cout<<"There is some changes for second lol ez wtf gg"<<std::endl;
+	std::cout<<"1"<<std::endl;
+	std::cout<<"2"<<std::endl;
+	std::cout<<"3"<<std::endl;
 }
